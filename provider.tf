d5daf8eccde443cf480c984c81a1ada8ee8dffaa
Will Schultz <will.schultz@ibm.com>
2022-10-06 14:48:37 -0500

    updating provider to use an app reg identity

--- provider.tf
+++ provider.tf
@@ -7,4 +7,9 @@ terraform {
   }
 }
 
-provider "azurerm" {}
+provider "azurerm" {
+  subscription_id = "d09f4363-eae7-4c79-864e-08154540c083"
+  client_id = "5a225ef9-1f2a-42e3-9efa-fa047567720c"
+  client_secret = var.client_secret
+  tenant_id = "c5614891-414a-4db9-8504-3fab2dfb9ff3"
+}
