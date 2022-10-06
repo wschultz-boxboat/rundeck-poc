4ac8ab3590a02b83217b7b69fedf0661d4630acb
Will Schultz <will.schultz@ibm.com>
2022-10-06 15:21:43 -0500

    fixing features

--- provider.tf
+++ provider.tf
@@ -8,6 +8,8 @@ terraform {
 }
 
 provider "azurerm" {
+  features {}
+
   subscription_id = "d09f4363-eae7-4c79-864e-08154540c083"
   client_id = "5a225ef9-1f2a-42e3-9efa-fa047567720c"
   client_secret = var.client_secret
